daf56412e6da2047ac3c389b43647c312ae9b187
Celso Bertolotto <celsonyl@hotmail.com>
2021-08-10 13:44:17 -0300

    first commit

---
+++ src/main/java/com/nyller/springmcclean/SpringMcCleanApplication.java
@@ -0,0 +1,13 @@
+package com.nyller.springmcclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMcCleanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMcCleanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyller/springmcclean/SpringMcCleanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyller.springmcclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMcCleanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
