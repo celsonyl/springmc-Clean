da4f9d58b356b108dbd7ebaaa7baa83a524d11d1
Celso Bertolotto <celsonyl@hotmail.com>
2021-08-10 15:21:21 -0300

    populate db with start project

--- src/main/java/com/nyller/springmcclean/SpringMcCleanApplication.java
+++ src/main/java/com/nyller/springmcclean/SpringMcCleanApplication.java
@@ -1,13 +1,29 @@
 package com.nyller.springmcclean;
 
+import com.nyller.springmcclean.gateway.h2database.model.CategoryDatabase;
+import com.nyller.springmcclean.gateway.h2database.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringMcCleanApplication {
+public class SpringMcCleanApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringMcCleanApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        CategoryDatabase category1 = new CategoryDatabase(null, "Escritório");
+        CategoryDatabase category2 = new CategoryDatabase(null, "Informática");
+        categoryRepository.saveAll(Arrays.asList(category1, category2));
+    }
 }
